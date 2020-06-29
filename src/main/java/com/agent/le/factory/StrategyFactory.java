a6ab2aabe0fe957e4f29c6ce5593ffb8ec0a03b9
wanghaidong01 <wanghaidong01@meicai.cn>
2020-06-29 23:47:06 +0800

    change

--- src/main/java/com/agent/le/factory/StrategyFactory.java
+++ src/main/java/com/agent/le/factory/StrategyFactory.java
@@ -14,7 +14,7 @@ import com.agent.le.strategy.MethodStrategyBody;
  */
 public class StrategyFactory {
     /**
-     * 策略工厂可以使用 注解+反射 来消除if 但是反射好性能……
+     * 策略工厂可以使用 注解+反射 来消除if 但是反射耗性能……
      *
      * @param configInfoDTO
      * @return
